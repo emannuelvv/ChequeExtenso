c36129ab8c2801bae173364979c1c798bdb8d4c6
emannuelvv <54732031+emannuelvv@users.noreply.github.com>
2021-05-26 11:49:37 -0300

    upando projeto, outra versao

---
+++ ChequeExtenso/ChequePorExtenso/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChequePorExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(new NumeroPorExtenso(5.22).NumeroExtenso);
+
+        }
+    }
+}
